806277de0890f5258066c602ff8233b559acf406
gunjianpan <iofu728@163.com>
2018-11-18 15:49:46 +0800

    :jeans: NN.RNN

---
+++ utils/constant.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: gunjianpan
+# @Date:   2018-11-18 10:55:14
+# @Last Modified by:   gunjianpan
+# @Last Modified time: 2018-11-18 10:56:32
+
+"""
+some constant variable
+"""
+
+import theano
+
+floatX = theano.config.floatX
+float32 = 'float32'
